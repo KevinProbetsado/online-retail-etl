45cb5dd94d9d83c9ff682eff59efb4162b42bb64
KevinProbetsado <167767657+KevinProbetsado@users.noreply.github.com>
2026-04-19 06:06:59 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from src.extract import extract_data
+from src.transform import transform_data
+from src.load import load_to_csv, load_to_postgres
+
+filepath = "data/Online_Retail.csv"
+
+#extract
+df = extract_data(filepath)
+
+#Transform
+df_clean = transform_data(df)
+
+#print(df_clean.head())
+
+# Step 3 - Load to CSV
+load_to_csv(df_clean)
+
+# Step 4 - Load to PostgreSQL
+load_to_postgres(
+    df_clean,
+    db_name='online_retail',
+    user='postgres',
+    password='Kevin#1234'  # ← put your pgAdmin password
+)
